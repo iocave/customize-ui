8ea9dc293fd8ed6104e054936e6c2e84d16174ab
Matej Knopp <matej.knopp@gmail.com>
2019-06-06 02:15:48 +0200

    Initial commit

---
+++ data/customize-font.js
@@ -0,0 +1,191 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+
+let override = function (where, name, cb) {
+    (function (original) {
+        where.prototype[name] = function () {
+            let t = this;
+            let a = arguments;
+            let res = cb.apply(this,
+                [() => original.apply(t, a), a]
+            );
+            return res;
+        }
+    })(where.prototype[name]);
+}
+
+let addStyleSheet = function (url) {
+    let head = document.getElementsByTagName('head')[0];
+    let link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = url;
+    link.media = 'all';
+    head.appendChild(link);
+}
+
+let addStyle = function (style) {
+    let css = document.createElement('style');
+    css.type = 'text/css';
+
+    css.appendChild(document.createTextNode(style));
+    document.getElementsByTagName("head")[0].appendChild(css);
+}
+
+define([
+    "module",
+    "require",
+    "vs/workbench/browser/workbench",
+    "vs/workbench/contrib/files/browser/views/explorerViewer",
+    "vs/base/browser/ui/splitview/panelview",
+    "vs/workbench/contrib/files/browser/views/openEditorsView",
+    "vs/editor/contrib/documentSymbols/outlineTree",
+    "vs/workbench/browser/parts/views/customView",
+    "vs/workbench/contrib/debug/browser/variablesView",
+    "vs/workbench/contrib/debug/browser/callStackView",
+    "vs/workbench/contrib/debug/browser/watchExpressionsView",
+    "vs/workbench/contrib/debug/browser/loadedScriptsView",
+    "vs/workbench/contrib/debug/browser/breakpointsView",
+    "vs/workbench/contrib/scm/browser/scmViewlet",
+    "vs/platform/configuration/common/configuration",
+], function (mod, req, workbench, explorerView, panelView, openEditorsView, outlineTree, customView,
+    variablesView, callStackView, watchExpressionsView, loadedScriptsView, breakpointsView, scm, configuration) {
+        'use strict';
+
+        addStyleSheet('file://' + req.toUrl(mod.id) + ".css");
+
+        let CustomizeFont = class CustomizeFont {
+            constructor(configurationService) {
+                this.configurationService = configurationService;
+
+                let rowHeight = this.configurationService.getValue("customizeUI.listRowHeight") || 22;
+                this.updateRowHeight(rowHeight);
+
+                let fontSize = this.configurationService.getValue("customizeUI.fontSizeMap") || {};
+                this.updateFontSize(fontSize);
+            }
+
+            updateFontSize(fontSizeMap) {
+                console.log(fontSizeMap);
+
+                let getFontSize = function (key) {
+                    let res = fontSizeMap[key];
+                    if (res === undefined)
+                        res = key;
+                    if (res === "monospace")
+                        res = "13px";
+                    if (res === "window-title")
+                        res = getFontSize("12px");
+                    if (res === "tab-title")
+                        res = getFontSize("13px");
+                    return res;
+                }
+
+                addStyle(`:root {
+                    --font-size-16: ${getFontSize("16px")};
+                    --font-size-15: ${getFontSize("15px")};
+                    --font-size-14: ${getFontSize("14px")};
+                    --font-size-13: ${getFontSize("13px")};
+                    --font-size-12: ${getFontSize("12px")};
+                    --font-size-11: ${getFontSize("11px")};
+                    --font-size-10: ${getFontSize("10px")};
+                    --font-size-9: ${getFontSize("9px")};
+                    --font-size-monospace: ${getFontSize("monospace")};
+                    --font-size-window-title: ${getFontSize("window-title")};
+                    --font-size-tab-title: ${getFontSize("tab-title")};
+                    }`);
+            }
+
+            updateRowHeight(rowHeight) {
+                // explorer view
+                explorerView.ExplorerDelegate.ITEM_HEIGHT = rowHeight;
+
+                // panel height (OUTLINE, DEPENDENCIES, ...)
+                panelView.Panel.HEADER_SIZE = rowHeight;
+
+                addStyle(`:root { --row-height: ${rowHeight}px; }`);
+
+                // open editors
+                override(openEditorsView.OpenEditorsView, "renderBody", function (original) {
+                    let res = original();
+                    this.list.view.virtualDelegate.__proto__.constructor.ITEM_HEIGHT = rowHeight;
+                    this.updateSize();
+                    return res;
+                });
+
+                // outline
+                outlineTree.OutlineVirtualDelegate.prototype.getHeight = function () {
+                    return rowHeight;
+                };
+
+                // custom views in sidebar
+                override(customView.CustomTreeView, "createTree", function (original) {
+                    let res = original();
+                    if (this.tree !== undefined) {
+                        this.tree.context.configuration.renderer.__proto__.constructor.ITEM_HEIGHT = rowHeight;
+                    }
+                    return res;
+                });
+
+                //
+                // Debugger
+                //
+
+                let replacement = function (original) {
+                    let res = original();
+                    this.tree.tree.view.view.virtualDelegate.getHeight = function () {
+                        return rowHeight;
+                    }
+                    return res;
+                };
+
+                override(variablesView.VariablesView, "renderBody", replacement);
+                override(callStackView.CallStackView, "renderBody", replacement);
+                override(watchExpressionsView.WatchExpressionsView, "renderBody", replacement);
+                override(loadedScriptsView.LoadedScriptsView, "renderBody", replacement);
+                override(breakpointsView.BreakpointsView, "renderBody", function (original) {
+                    let res = original();
+                    this.list.view.virtualDelegate.getHeight = function () {
+                        return rowHeight;
+                    }
+                    return res;
+                });
+
+                //
+                // SCM
+                //
+
+                replacement = function (original) {
+                    let res = original();
+                    this.list.view.virtualDelegate.getHeight = function () {
+                        return rowHeight;
+                    }
+                    return res;
+                }
+
+                override(scm.MainPanel, "renderBody", replacement);
+                override(scm.RepositoryPanel, "renderBody", replacement);
+            }
+        }
+
+        CustomizeFont = __decorate([
+            __param(0, configuration.IConfigurationService)
+        ], CustomizeFont);
+
+        let haveInstantiationService = function (service) {
+            let conf = service.createInstance(CustomizeFont);
+        }
+
+        override(workbench.Workbench, "renderWorkbench", function (original, args) {
+            let res = original();
+            haveInstantiationService(args[0]);
+            return res;
+        });
+    });
---
+++ src/extension.ts
@@ -0,0 +1,86 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as path from 'path';
+
+interface FolderMap { [key: string]: string; }
+
+interface Contribution {
+	folderMap: FolderMap;
+	browserModules: Array<string>;
+	mainProcessModules: Array<string>;
+}
+
+interface API {
+	contribute(sourceExtensionId: string, contribution: Contribution): void;
+	active(): boolean;
+}
+
+class Extension {
+
+	constructor(context: vscode.ExtensionContext) {
+		this.context = context;
+
+		context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(e => {
+			if (e.affectsConfiguration('customizeUI.')) {
+				this.configurationChanged();
+			}
+		}));
+	}
+
+	get dataPath() {
+		return path.join(this.context.extensionPath, "data");
+	}
+
+	async start() {
+		let monkeyPatch = vscode.extensions.getExtension("iocave.monkey-patch");
+
+		if (monkeyPatch !== undefined) {
+			await monkeyPatch.activate();
+			let exports: API = monkeyPatch.exports;
+			exports.contribute("iocave.customize-ui",
+				{
+					folderMap: {
+						"customize-ui": this.dataPath,
+					},
+					browserModules: [
+						"customize-ui/customize-font"
+					],
+					mainProcessModules: [
+					]
+				}
+			);
+		} else {
+			vscode.window.showWarningMessage("Monkey Patch extension is not installed. CustomizeUI will not work.");
+		}
+	}
+
+	async configurationChanged() {
+		let monkeyPatch = vscode.extensions.getExtension("iocave.monkey-patch");
+		if (monkeyPatch !== undefined) {
+			await monkeyPatch.activate();
+			let exports: API = monkeyPatch.exports;
+			if (!exports.active()) {
+				let res = await vscode.window.showWarningMessage("Monkey Patch extension is not enabled. Please enable Monkey Patch in order to use Customize UI.", "Enable");
+				if (res === "Enable") {
+					vscode.commands.executeCommand("iocave.monkey-patch.enable");
+				}
+			}
+			let res = await vscode.window.showInformationMessage("Customizing UI requires window reload", "Reload Window");
+			if (res === "Reload Window") {
+				vscode.commands.executeCommand("workbench.action.reloadWindow");
+			}
+		}
+	}
+
+	context: vscode.ExtensionContext;
+}
+
+
+export function activate(context: vscode.ExtensionContext) {
+
+	new Extension(context).start();
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
